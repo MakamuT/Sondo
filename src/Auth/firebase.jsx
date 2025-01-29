d1273078ef881d79f587c612002a27fa7ebbb7c3
Thembisile  Makamu <makamuthe2@gmail.com>
2025-01-29 17:03:03 +0200

    refactor: update Firebase configuration to use environment variables and export Firestore

--- src/Auth/firebase.jsx
+++ src/Auth/firebase.jsx
@@ -1,24 +1,22 @@
-// Import the functions you need from the SDKs you need
+// Import the functions from the SDKs
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+//Firebase configuration (using environment variables)
 const firebaseConfig = {
-  apiKey: "AIzaSyARXw1vcUTplDNLdndSSfoeXyi-xlCjLwo",
-  authDomain: "sondo-dc030.firebaseapp.com",
-  projectId: "sondo-dc030",
-  storageBucket: "sondo-dc030.firebasestorage.app",
-  messagingSenderId: "820810975062",
-  appId: "1:820810975062:web:f0339a76103d5591c9b8f7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-//Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Export the auth object
-export { auth };
+export { auth, db };
 export default app;
-export const db = getFirestore(app);
